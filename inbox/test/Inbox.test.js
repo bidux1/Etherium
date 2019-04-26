7759743ad73ab38351251c8c0f903afca99be97a
bidux1 <diepquoc@vncdevs.io>
2019-04-26 17:35:53 +0700

    Update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,29 +1,40 @@
-  const assert = require('assert');
-  const ganache = require('ganache-cli');
-  const Web3 = require('web3');
-  const web3 = new Web3(ganache.provider());
-  const {
-    interface,
-    bytecode
-  } = require('../compile');
-  let account, inbox;
-  beforeEach(async () => {
-    // get list of all account
-    account = await web3.eth.getAccounts();
-    // use one of account to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({
-        data: bytecode,
-        arguments: ['Hi there!']
-      })
-      .send({
-        from: account[0],
-        gas: '1000000'
-      })
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const INIT_MESSAGE = 'Hi there!';
+const {
+  interface,
+  bytecode
+} = require('../compile');
+let account, inbox;
+beforeEach(async () => {
+  // get list of all account
+  account = await web3.eth.getAccounts();
+  // use one of account to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [INIT_MESSAGE]
+    })
+    .send({
+      from: account[0],
+      gas: '1000000'
+    })
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INIT_MESSAGE)
+  });
+  it('has a default message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: account[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye')
   });
 
-  describe('Inbox', () => {
-    it('deploys a contract', () => {
-      console.log(inbox);
-    });
-  });
+});
