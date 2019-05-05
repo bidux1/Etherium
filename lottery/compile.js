3448c704f54ce4335e6028a12ca4c566bb18d168
bidux1 <dieptrungquoc1995@gmail.com>
2019-05-06 00:10:13 +0700

    update

---
+++ lottery/compile.js
@@ -0,0 +1,8 @@
+//require('./contracts/Inbox.sol'); bad
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
