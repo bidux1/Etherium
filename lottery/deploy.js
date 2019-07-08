20245cb10892c8543f0dda8322b693d9cf56e59f
bidux1 <diepquoc@vncdevs.io>
2019-07-09 01:20:19 +0700

    Update

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,30 +1,28 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const mnemonic = 'tape govern husband vacuum author axis sure bunker thing absurd wire face';
-const INIT_MESSAGE = 'Hi there!';
-const {
-    interface,
-    bytecode
-} = require('./compile');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const mnemonic =
+  "order hurdle vessel merit tag perfect notice arena moral runway force inside";
+const INIT_MESSAGE = "Hi there!";
+const { interface, bytecode } = require("./compile");
 const provider = new HDWalletProvider(
-    mnemonic,
-    'https://eth-rinkeby.alchemyapi.io/jsonrpc/39jSoRSmK1OG4OKOB3Xqtlu0ib2mPGXx'
+  mnemonic,
+  "https://eth-rinkeby.alchemyapi.io/jsonrpc/39jSoRSmK1OG4OKOB3Xqtlu0ib2mPGXx"
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
-    console.log('Attempting to deploy from account', accounts[0]);
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({
-            data: bytecode
-        })
-        .send({
-            from: accounts[0],
-            gas: '1000000'
-        });
-    console.log(interface);
-    console.log('Contract deployed to', result.options.address);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000"
+    });
+  console.log(interface);
+  console.log("Contract deployed to", result.options.address);
 };
 deploy();
